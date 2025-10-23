0ab666d1d1477821463f382f5004680f1add228a
Mohamed Ibrahim <152276499+mohamedmisho22@users.noreply.github.com>
2025-10-23 22:45:45 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# untitled9
+# untitled11
 
 A new Flutter project.
 
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\moham\Downloads\flutter_windows_3.29.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\moham\AndroidStudioProjects\untitled9"
+export "FLUTTER_APPLICATION_PATH=C:\Users\moham\AndroidStudioProjects\untitled11"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
